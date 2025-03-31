3211a2b6914d013d2463316636540055862c3240
abelm19 <abelmetek19@gmail.com>
2025-03-31 17:19:50 +0800

    added gitignore

--- app/app.py
+++ app/app.py
@@ -2,14 +2,62 @@
 import streamlit as st
 #Useful for loading external web based scripts or documents into the LLM
 from langchain_community.document_loaders import WebBaseLoader
-
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
+import base64
+
 
 def create_streamlit_app(llm, portfolio, clean_text):
     #title for the web app
-    st.title("📧 Cold Mail Generator App")
+    st.title("📧 Cold Mail Generator")
+    image_path = 'background.jpg'  # Specify your image file path
+    with open(image_path, "rb") as img_file:
+        img_base64 = base64.b64encode(img_file.read()).decode()
+
+    # Inject custom CSS for background and text
+    st.markdown(
+        f"""
+            <style>
+            .stApp {{
+                background-image: url('data:image/jpeg;base64,{img_base64}');
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+            }}
+
+            .custom-text-box {{
+                background-color: rgba(0, 0, 0, 0.7);  /* Dark background */
+                color: white;  /* White text */
+                padding: 20px;
+                border-radius: 10px;
+                font-size: 18px;
+            }}
+
+            body {{
+                color: black !important;
+            }}
+            .streamlit-expanderHeader {{
+                color: black !important;
+            }}
+            h1, h2, h3, h4, h5, h6 {{
+                color: black !important;
+            }}
+            .css-1d391kg {{
+                color: black !important;
+            }}
+            .css-1emrehy {{
+                color: black !important;
+            }}
+            /* Change form labels to black */
+            label {{
+                color: black !important;
+            }}
+             /* Target radio button labels specifically */
+            </style>
+            """,
+        unsafe_allow_html=True
+    )
     url_input = st.text_input("Enter a URL:", value="https://careers.nike.com/lead-data-scientist/job/R-56462")
     submit_button = st.button("Submit")
 
@@ -31,5 +79,5 @@ def create_streamlit_app(llm, portfolio, clean_text):
 if __name__ == "__main__":
     chain = Chain()
     portfolio = Portfolio()
-    st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
+    #st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
     create_streamlit_app(chain, portfolio, clean_text)
--- app/chains.py
+++ app/chains.py
@@ -37,22 +37,19 @@ class Chain:
     def write_mail(self, job, links):
         prompt_email = PromptTemplate.from_template(
             """
-            ### JOB DESCRIPTION:
-            {job_description}
+             ### JOB DESCRIPTION:
+                {job_description}
 
             ### INSTRUCTION:
-            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
-            the seamless integration of business processes through automated tools. 
-            Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
-            process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
-            in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Mohan, BDE at AtliQ. 
+            You are a business development executive at a leading AI & Software Consulting company. Your company specializes in facilitating the seamless integration of business processes through automated tools.
+            Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, process optimization, cost reduction, and heightened overall efficiency.
+            Your job is to write a cold email to the client regarding the job mentioned above, describing the capability of your company in fulfilling their needs.
+            Also, add the most relevant ones from the following links to showcase your company's portfolio: {link_list}
+            Address the client professionally, without mentioning specific names.
             Do not provide a preamble.
-            ### EMAIL (NO PREAMBLE):
 
-            """
+            ### EMAIL (NO PREAMBLE):
+    """
         )
         chain_email = prompt_email | self.llm
         res = chain_email.invoke({"job_description": str(job), "link_list": links})
